8a840e5bfdc94f77b294e7d621a3603eaed92a08
Jean-PierreC <eaglejean11@gmail.com>
2020-10-16 19:01:34 -0700

    new code

---
+++ AdvancedProjects/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,78 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.PWMTalonSRX;
+
+import frc.robot.RobotMap;
+import frc.robot.OI;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+
+/**
+ * Add your docs here.
+ */
+public class DriveTrain extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  private PWMTalonSRX left = new PWMTalonSRX(RobotMap.leftDrivePort), right = new PWMTalonSRX(RobotMap.rightDrivePort), arm= new PWMTalonSRX(RobotMap.liftPort);
+  public static DriveTrain drive;
+  private boolean downArm, upArm;
+
+  public DriveTrain() {
+    left.setInverted(true);
+    
+  }
+
+  public static DriveTrain getInstance() {
+    if (drive == null) {
+      drive = new DriveTrain();
+    }
+    return drive;
+  }
+
+  public void tankDrive(double leftPow, double rightPow) {
+
+    //Minimizing error from small inputs
+    if (leftPow < 0.05 && leftPow > -0.05) {
+      leftPow = 0;
+    }
+    if (rightPow < 0.05 && rightPow > -0.05) {
+      rightPow = 0;
+    }
+
+    left.set(leftPow);
+    right.set(rightPow);
+  }
+  
+
+ 
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+  @Override
+  public void periodic() {
+    downArm= OI.returnController().getRawButton(RobotMap.rightBumper);
+    upArm= OI.returnController().getRawButton(RobotMap.rightBumper);
+    if(downArm){
+      arm.set(-.3);
+    }
+    if(downArm){
+      arm.set(.3);
+    }
+    else{
+      arm.set(0);
+    }
+    tankDrive(OI.returnController().getRawAxis(RobotMap.leftJoystick) * 0.3, OI.returnController().getRawAxis(RobotMap.rightJoystick) * 0.3);
+  }
+}
